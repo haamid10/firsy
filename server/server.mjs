fb3615c0d571d1711bc9177bf31a034d05396800
haamid10 <apdixamiid919@gmail.com>
2023-04-23 20:42:07 +0300

    d

--- server/server.mjs
+++ server/server.mjs
@@ -11,5 +11,14 @@ app.use(morgan('dev'))
 const db = []
 
 app.post('/todo' ,(req,res)=> {
+const newTodo ={
+  id: Date.now(),
+  text: req.body.text
+}
+db.push(newTodo)
+res.json(newTodo)
+})
 
+app.get ('/todo', (req,res)=>{
+  res.json(db)
 })
