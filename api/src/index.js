5cb1fc4b6002483f6e69fcf3a819fd3d4c583500
haamid10 <apdixamiid919@gmail.com>
2023-04-25 17:38:22 +0300

    http

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,8 @@ const http = require('http')
 
 const server = http.createServer(async(req,res) => {
   if (req.method === 'GET' && req.url === '/'){
-    // res.statusCode(200)
+   console.log('Hello from server')
+
     res.end()
   }
 })
