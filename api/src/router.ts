b7605f549154746bdaa52d2f1cbe2348a9ab55c2
haamid10 <apdixamiid919@gmail.com>
2023-04-29 21:35:50 +0300

    middleware

--- api/src/router.ts
+++ api/src/router.ts
@@ -2,7 +2,9 @@ import {Router} from 'express'
 
 const router = Router()
 
-router.get ('/product', ()=>{})
+router.get ('/product', (req,res)=>{
+  res.json({message: 'hello'})
+})
 router.get ('/product/:id', ()=>{})
 router.put ('/product/:id', ()=>{})
 router.post ('/product', ()=>{})
