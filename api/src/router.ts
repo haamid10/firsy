1720a3ce3cb1c61b30944e24c08179e5f574764f
haamid10 <apdixamiid919@gmail.com>
2023-04-29 20:21:11 +0300

    msking  Routers

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,3 +1,25 @@
-import express from 'express'
+import {Router} from 'express'
 
-const app = express
+const router = Router()
+
+Router.get ('/product', ()=>{})
+Router.get ('/product/:id', ()=>{})
+Router.put ('/product/:id', ()=>{})
+Router.post ('/product', ()=>{})
+Router.delete ('/product/:id', ()=>{})
+
+// updates
+
+Router.get ('/update', ()=>{})
+Router.get ('/update/:id', ()=>{})
+Router.put ('/update/:id', ()=>{})
+Router.post ('/update', ()=>{})
+Router.delete ('/update/:id', ()=>{})
+
+// update points
+
+Router.get ('/updatepiont', ()=>{})
+Router.get ('/updatepiont/:id', ()=>{})
+Router.put ('/updatepiont/:id', ()=>{})
+Router.post ('/updatepiont', ()=>{})
+Router.delete ('/updatepiont/:id', ()=>{})
