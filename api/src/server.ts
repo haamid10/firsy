f22d9583f99a705002cac4fc6489d42694f1944e
haamid10 <apdixamiid919@gmail.com>
2023-04-29 21:28:39 +0300

    middleware

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import router from './router'
+import morgan from 'morgan'
 
 const app= express()
 
-// app.get('/',(req,res)=>{
-//   console.log('hello from express')
-//   res.status(200)
-//   res.json({message: 'hello'})
-// })
+app.use(morgan('dev'))
 
+app.get('/',(req,res)=>{
+  console.log('hello from express')
+  res.status(200)
+  res.json({message: 'hello'})
+})
 app.use('/api', router)
 
 
