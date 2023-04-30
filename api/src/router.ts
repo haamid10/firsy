833d7d0e3e45f592bfdb9239569a31af4ca688d8
haamid10 <apdixamiid919@gmail.com>
2023-04-30 19:12:35 +0300

    finally fixed

--- api/src/router.ts
+++ api/src/router.ts
@@ -3,7 +3,7 @@ import {Router} from 'express'
 const router = Router()
 
 router.get ('/product', (req,res)=>{
-  res.json({message: req.shhh_ })
+  res.json({message: req.body })
 })
 router.get ('/product/:id', ()=>{})
 router.put ('/product/:id', ()=>{})
