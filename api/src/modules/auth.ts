f86bae6de50863bb1d8127a3f8ff48780a93115f
haamid10 <apdixamiid919@gmail.com>
2023-05-01 08:37:57 +0300

    error

--- api/src/modules/auth.ts
+++ api/src/modules/auth.ts
@@ -1,16 +1,9 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const createJWT = (user) => {
-  const token = jwt.sign({id: user.id,username:user.username},process.env.JWT_SECRET)
-
-  return token
-}
-
-export const protect = (req,res) => {
-  const bearer = req.headers.authorization;
-  if (!bearer) {
-    res.status(401)
-    res.json({message: 'not authorized'})
-    return
-  }
-}
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
