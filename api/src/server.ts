cee566d7ad735408d09d8b5a42c4fd84756b847f
haamid10 <apdixamiid919@gmail.com>
2023-05-01 08:38:06 +0300

    gui

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app= express()
 app.use(cors())
@@ -16,7 +17,7 @@ res.send('Nope')
 })
 
 //  
-app.use('/api', router)
+app.use('/api',protect, router)
 
 
 export default  app
